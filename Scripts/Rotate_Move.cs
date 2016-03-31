9eb131dd39680f7fdfffc7142ca8fd469dab2632
git.Theotim <theotimdelan@hotmail.fr>
2016-03-31 08:19:14 +0200

    Jenaimardelavi
    Un commit de pokemon

---
+++ Scripts/Rotate_Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate_Move : MonoBehaviour {
+	private GameObject target;
+	public float speed;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		target=GameObject.Find("patoune");
+		float step = speed*Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.transform.position, step);
+	}
+}
